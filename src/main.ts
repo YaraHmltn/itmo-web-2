2fc61cb0ef8c8c484fc4b4134f97f32a7429cb79
Ярослава Исаева <yaroslavavdovina@icloud.com>
2023-09-15 01:01:37 +0300

    2 step

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { NestFactory } from '@nestjs/core';
+import { ConfigService } from '@nestjs/config';
+import { NestExpressApplication} from "@nestjs/platform-express";
+import { join } from 'path';
+import { AppModule } from './app.module';
+
+
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+
+  app.useStaticAssets(join(__dirname, '..', 'public'));
+
+  const port = app.get(ConfigService).get<number>('PORT')|| 12345;
+  await app.listen(port);
+}
+bootstrap();
