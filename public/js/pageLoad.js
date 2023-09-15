8a8c965a27e552be333b39bb9c120eb082980812
YaraHmltn <yaroslavavdovina@icloud.com>
2023-09-15 19:28:54 +0300

    step 3.0

---
+++ public/js/pageLoad.js
@@ -0,0 +1,17 @@
+(function () {
+  let startTime = new Date().getTime();
+
+  function timer() {
+    topbar.show();
+    let element = document.getElementById('load-time');
+    element.innerHTML +=
+      'page loaded in ' +
+      (new Date().getTime() - startTime) / 1000 +
+      ' seconds';
+  }
+
+  document.addEventListener('DOMContentLoaded', function () {
+    timer();
+    topbar.hide();
+  });
+})();
--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,17 @@ async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const hbs = require('hbs');
+  hbs.registerPartials(join(__dirname, '..', 'views', 'partials'));
+  hbs.registerHelper('times', function (n, block) {
+    let accum = '';
+    for (let i = 1; i <= n; ++i) accum += block.fn(i);
+    return accum;
+  });
 
   const port = app.get(ConfigService).get<number>('PORT') || 12345;
   await app.listen(port);
