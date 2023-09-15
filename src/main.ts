f5a20cad26faa0a5624646d4f7543cf0a8c91858
YaraHmltn <yaroslavavdovina@icloud.com>
2023-09-15 04:08:58 +0300

    step 2.2

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { ConfigService } from '@nestjs/config';
-import { NestExpressApplication} from "@nestjs/platform-express";
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
-
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.useStaticAssets(join(__dirname, '..', 'public'));
 
-  const port = app.get(ConfigService).get<number>('PORT')|| 12345;
+  const port = app.get(ConfigService).get<number>('PORT') || 12345;
   await app.listen(port);
 }
 bootstrap();
