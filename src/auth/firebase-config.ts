15d681b9ebe7a2e5ddabee99aa8b223a60cd0f86
YaraHmltn <yaroslavavdovina@icloud.com>
2023-10-10 21:40:22 +0300

    rollback

--- src/auth/firebase-config.ts
+++
@@ -1,7 +0,0 @@
-const firebaseConfig = {
-    projectId: process.env.PROJECT_ID,
-    clientEmail: process.env.CLIENT_EMAIL,
-    privateKey: process.env.PRIVATE_KEY.replace(/\\n/gm, '\n'),
-};
-
-export default firebaseConfig;
